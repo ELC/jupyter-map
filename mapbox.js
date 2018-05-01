fe421e5e26040eec2bbc88fe3cceb6af394bca21
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-04-30 21:06:30 -0300

    :tada: Initial commit

---
+++ mapbox.js
@@ -0,0 +1,170 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWxjZyIsImEiOiJjamdtemlpbXMwMDZjMnBwOWM0ZmpydDZxIn0.w5gGuIql9j-j5TADhBatHg';
+
+var center =  [10, 20]
+var initial = [ -60.643775, -32.954626]
+var zoom = 1.5
+var url = 'https://raw.githubusercontent.com/ELC/jupyter-map/master/jupyter-map.geojson';
+var places; // Get value async below
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: initial,
+    zoom: 4,
+    minZoom: zoom
+});
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+
+var places;
+// Add data
+
+async function addData() {
+    const response = await fetch(url);
+    places = await response.json();
+
+    // Add a layer of places from external file
+    map.addSource('data', { type: 'geojson', data: places });
+    map.addLayer({
+        "id": "places",
+        "type": "symbol",
+        "source": 'data',
+        "layout": {
+            "icon-image": "marker-15",
+            "icon-allow-overlap": true,
+            "icon-size": 1.5,
+            "text-field": "",
+            'text-allow-overlap': true,
+            'text-size': 16,
+            "text-letter-spacing": 0.05,
+            "text-offset": [0, 2]
+        }
+    });
+
+    });
+
+}
+
+// First time
+
+map.on('load', () => {
+    // Initial Animation
+
+    setTimeout(() => {
+        map.flyTo({
+            center: center,
+            zoom: zoom,
+            speed: 1
+        });
+        }, 500)
+
+    // Style Menu
+    let layerList = document.getElementById('menu');
+    let inputs = layerList.getElementsByTagName('input');
+
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].onclick = switchLayer;
+    }
+
+});
+
+// Load data every time the style is change and also first time
+
+map.on('style.load', addData);
+
+function switchLayer(layer) {
+    let layerId = layer.target.id;
+    let version = '-v9'
+    if (layerId.includes('traffic')){
+        version = '-v2'
+    }
+    map.setStyle('mapbox://styles/mapbox/' + layerId + version);
+}
+
+
+// PopUp on Hover
+
+var popup = new mapboxgl.Popup();
+
+map.on('mouseenter', 'places', function(e) {
+    // Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = 'pointer';
+
+    // Create a popup, but don't add it to the map yet.
+
+    let coordinates = e.features[0].geometry.coordinates.slice();
+    let properties = e.features[0].properties;
+
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    // Populate the popup and set its coordinates based on the features found.
+
+    let contactInfo = `
+        <p><strong>Contact: </strong>${properties.contact_name}</p>
+        <p><strong>Contact Email: </strong><a href="mailto:${properties.contact_email}" target="_blank">${properties.contact_email}</a></p>`
+
+    if (properties.instructor_name && 0 !== properties.instructor_name.length){
+        if (properties.contact_name.localeCompare(properties.instructor_name) !== 0){
+            contactInfo = `
+                <p><strong>Instructor: </strong>${properties.instructor_name}</p>
+                <p><strong>Instructor Email: </strong><a href="mailto:${properties.contact_email}" target="_blank">${properties.instructor_email}</a></p>
+                ${contactInfo}`
+        } else{
+            contactInfo = `
+                <p><strong>Instructor: </strong>${properties.instructor_name}</p>
+                <p><strong>Instructor Email: </strong><a href="mailto:${properties.contact_email}" target="_blank">${properties.instructor_email}</a></p>`
+        }
+    }
+
+    popup.setLngLat(coordinates)
+        .setHTML(`
+            <h2><a href="${properties.institution_url}" target="_blank">${properties.institution_name}</a></h2>
+            <p><strong>Course Name: </strong> <a href="${properties.course_url}" target="_blank">${properties.course_name}</a></p>
+            <p><strong>Course Area: </strong> ${properties.course_area}</p>
+            ${contactInfo}`
+        )
+        .addTo(map);
+});
+
+// Fly on click
+
+map.on('click', 'places', function (e) {
+    map.flyTo({
+        center: e.features[0].geometry.coordinates,
+        zoom:16
+    });
+});
+
+// Auto Display text based on Zoom
+
+map.on('zoomend', function(e) {
+    map.setLayoutProperty('places', "text-field",
+        map.getZoom() > 4.5 ? "{institution_name}" : "" )
+});
+
+// Modify Cursor
+
+map.on('mouseleave', 'places', function(e) {
+    map.getCanvas().style.cursor = '';
+});
+
+map.on('click', function(e) {
+    map.getCanvas().style.cursor = '';
+});
+
+// Reset Button
+
+document.getElementById('reset').addEventListener('click', function() {
+    map.setLayoutProperty('places', "text-field", "")
+    map.flyTo({
+        center: center,
+        zoom: zoom,
+        speed: 1.2
+    });
+});
+
+
